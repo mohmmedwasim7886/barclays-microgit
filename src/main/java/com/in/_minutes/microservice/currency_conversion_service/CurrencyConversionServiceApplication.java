4ec66414b39d2f3fa561d4d648be18ee00549f79
mohmmedwasim7886 <waseemkacchi@gmail.com>
2026-03-29 17:11:43 +0530

    this is new commit folder added

---
+++ src/main/java/com/in/_minutes/microservice/currency_conversion_service/CurrencyConversionServiceApplication.java
@@ -0,0 +1,16 @@
+package com.in._minutes.microservice.currency_conversion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		//this is my commit from STS
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+		String value="";//hello sdfg sdf  sdfg
+		String str="";
+	}
+
+}
